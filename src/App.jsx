c60298c9c93be8aefa832c386225db5f08543cbc
AnaCouto1409 <35375044+AnaCouto1409@users.noreply.github.com>
2022-06-07 15:47:25 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import logo from '../carro.jpg';
+import logo from './carro.jpg';
+import header from './header/header'
+import footer from './footer/footer'
+import pages from 'react-dom'
 import './App.css';
 
 function App() {
@@ -7,18 +10,27 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <p>
-          Click <code>HERE</code> and go to vehicles
+        <p href="https://mockapi.io/clone/629e0cb9c6ef9335c0ad6581">
+         <button>Click</button> in my button <code>HERE</code> and go to vehicles
         </p>
         
         <a
           className="App-link"
-          href="https://cidadao2.sinesp.gov.br/api/vehicles/${placa}"
+          href="https://629e0cb9c6ef9335c0ad6580.mockapi.io/auto/vehiclesResilia"
           target="_blank"
           rel="noopener noreferrer">
+            
           vehicles
         </a>
-        
+       
+        <summary>
+          <span>Aqui tem os melhores carros!</span>
+          <pre><code>
+          
+          </code>
+
+          </pre>
+        </summary>
       </header>
     </div>
   );
